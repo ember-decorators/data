55d41eb21d172515da849efa68cd15db77ff9318
James C. Davis <james@cos.io>
2018-05-09 12:51:14 -0400

    Add options and s/ModelRegistry/TransformRegistry/ for attr type

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-import { ModelRegistry } from "ember-data";
+import DS, { ModelRegistry, TransformRegistry } from "ember-data";
 
 /**
  * Decorator that turns the property into an Ember Data attribute
@@ -15,40 +15,11 @@ import { ModelRegistry } from "ember-data";
  * @function
  * @param {String} [type] - Type of the attribute
  */
-export function attr(): PropertyDecorator;
-/**
- * Decorator that turns the property into an Ember Data attribute
- *
- * ```javascript
- * import Model from 'ember-data/model';
- * import { attr } from 'ember-decorators/data';
- *
- * export default Model.extend({
- *   @attr firstName: null
- * });
- * ```
- *
- * @function
- * @param {String} [type] - Type of the attribute
- */
-export function attr<K extends keyof ModelRegistry>(type: K): PropertyDecorator;
+export function attr<K extends keyof TransformRegistry>(
+  type?: K,
+  options?: DS.AttrOptions<TransformRegistry[K]>,
+): PropertyDecorator;
 
-/**
- * Decorator that turns the property into an Ember Data `hasMany` relationship
- *
- * ```javascript
- * import Model from 'ember-data/model';
- * import { hasMany } from 'ember-decorators/data';
- *
- * export default Model.extend({
- *   @hasMany users: null
- * });
- * ```
- *
- * @function
- * @param {String} [type] - Type of the `hasMany` relationship
- */
-export function hasMany(): PropertyDecorator;
 /**
  * Decorator that turns the property into an Ember Data `hasMany` relationship
  *
@@ -65,7 +36,8 @@ export function hasMany(): PropertyDecorator;
  * @param {String} [type] - Type of the `hasMany` relationship
  */
 export function hasMany<K extends keyof ModelRegistry>(
-  type: K
+  type?: K,
+  options?: DS.RelationshipOptions<ModelRegistry[K]>
 ): PropertyDecorator;
 
 /**
@@ -82,20 +54,7 @@ export function hasMany<K extends keyof ModelRegistry>(
  * @function
  * @param {String} [type] - Type of the `belongsTo` relationship
  */
-export function belongsTo(): PropertyDecorator;
-/**
- * Decorator that turns the property into an Ember Data `belongsTo` relationship
- *
- * ```javascript
- * import Model from 'ember-data/model';
- * import { belongsTo } from 'ember-decorators/data';
- *
- * export default Model.extend({
- *   @belongsTo user: null
- * });
- * ```
- * @function
- * @param {String} [type] - Type of the `belongsTo` relationship
- */ export function belongsTo<K extends keyof ModelRegistry>(
-  type?: K
+export function belongsTo<K extends keyof ModelRegistry>(
+  type?: K,
+  options?: DS.RelationshipOptions<ModelRegistry[K]>
 ): PropertyDecorator;
